ef1dc6d260231f526971033390546807476e5200
II <hx37118@mail.ustc.edu.cn>
2017-01-23 01:04:38 +0800

    Artromino Initialized

---
+++ Assets/Scripts/Artris.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Artris : MonoBehaviour 
+{
+
+    public Transform currentArtromino;
+    public Vector3 spawnLocation;
+
+	// Use this for initialization
+	void Start () 
+    {
+        spawnLocation = new Vector3(0, 0, 5);
+        currentArtromino = SpawnRandomArtromino();
+        currentArtromino.parent = transform;
+	}
+	
+	// Update is called once per frame
+	void Update () 
+    {
+		
+	}
+
+    Transform SpawnRandomArtromino()
+    {
+        int rand = Random.Range(1, 8);
+        string spawnName = "";
+        switch (rand)
+        {
+            case 1:
+                spawnName = "I";
+                break;
+            case 2:
+                spawnName = "J0";
+                break;
+            case 3:
+                spawnName = "J1";
+                break;
+            case 4:
+                spawnName = "O";
+                break;
+            case 5:
+                spawnName = "T";
+                break;
+            case 6:
+                spawnName = "Z0";
+                break;
+            case 7:
+                spawnName = "Z1";
+                break;
+        }
+
+        return Instantiate(Resources.Load(spawnName), spawnLocation, new Quaternion(0, 0, 0, 0)) as Transform;
+    }
+
+    public void MoveForward() 
+    {
+    }
+
+    public void MoveBack() 
+    {
+    }
+
+    public void MoveLeft() 
+    {
+    }
+
+    public void MoveRight()
+    {
+    }
+}
