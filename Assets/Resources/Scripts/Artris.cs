35912ce67fa51b4a0c3ec1b2a2f64c3cf5917c0a
II <hx37118@mail.ustc.edu.cn>
2017-01-24 20:50:01 +0800

    Falling enabled & Scale corrected

--- Assets/Resources/Scripts/Artris.cs
+++ Assets/Resources/Scripts/Artris.cs
@@ -4,54 +4,65 @@ using UnityEngine;
 
 public class Artris : MonoBehaviour 
 {
-    public Vector3 spawnLocation;
-    Transform currentArtromino;
+    private Transform currentArtromino;
+    private int gridWidth = 10;
+    private int gridHeight = 20;
 
 	// Use this for initialization
 	void Start () 
     {
-        spawnLocation = new Vector3(0.1f, 4.1f, -0.1f);
-        currentArtromino = SpawnRandomArtromino();
-        currentArtromino.parent = transform;
+        SpawnRandomArtromino();
 	}
 	
 	// Update is called once per frame
 	void Update () 
     {
-		
+        if (Landed())
+        {
+            SpawnRandomArtromino();
+        }
 	}
 
-    Transform SpawnRandomArtromino()
+    public void SpawnRandomArtromino()
     {
         int rand = Random.Range(1, 8);
         string spawnName = "";
+        Vector3 spawnLocation = new Vector3(0.0f, 20.5f, -0.5f);
         switch (rand)
         {
             case 1:
                 spawnName = "Prefabs/I";
+                spawnLocation = new Vector3(0.0f, 20.5f, -0.5f);
                 break;
             case 2:
                 spawnName = "Prefabs/J0";
+                spawnLocation = new Vector3(0.5f, 20.5f, -0.5f);
                 break;
             case 3:
                 spawnName = "Prefabs/J1";
+                spawnLocation = new Vector3(-0.5f, 20.5f, -0.5f);
                 break;
             case 4:
                 spawnName = "Prefabs/O";
+                spawnLocation = new Vector3(0.0f, 20.5f, -0.5f);
                 break;
             case 5:
                 spawnName = "Prefabs/T";
+                spawnLocation = new Vector3(0.5f, 20.5f, -0.5f);
                 break;
             case 6:
                 spawnName = "Prefabs/Z0";
+                spawnLocation = new Vector3(0.5f, 20.5f, -0.5f);
                 break;
             case 7:
                 spawnName = "Prefabs/Z1";
+                spawnLocation = new Vector3(-0.5f, 20.5f, -0.5f);
                 break;
         }
-
+                
         GameObject SpawnObject = (GameObject)Instantiate(Resources.Load(spawnName), spawnLocation, new Quaternion(0, 0, 0, 0));
-        return  SpawnObject.transform;
+        currentArtromino = SpawnObject.transform;
+        currentArtromino.parent = transform;
     }
 
     public void MoveForward() 
@@ -82,5 +93,15 @@ public class Artris : MonoBehaviour
     public void HorizontalTransform()
     {
         currentArtromino.Rotate(0, 90, 0);
+    }
+        
+    bool Landed()
+    {
+        return false;
+    }
+
+    bool Collided()
+    {
+        return false;
     }
 }
--- Assets/Resources/Scripts/Artromino.cs
+++ Assets/Resources/Scripts/Artromino.cs
@@ -4,14 +4,26 @@ using UnityEngine;
 
 public class Artromino : MonoBehaviour {
 
+    private float deltaTime = 1.0f;
+    private float timeKey = 0.0f;
+
 	// Use this for initialization
 	void Start () {
-		
+        timeKey = Time.time;
 	}
 	
 	// Update is called once per frame
-	void Update () {
-		
+	void Update () 
+    {
+        Falling();
 	}
 
+    void Falling()
+    {
+        if (Time.time - timeKey >= deltaTime)
+        {
+            timeKey = Time.time;
+            transform.localPosition += new Vector3(0.0f, -1.0f, 0.0f);
+        }        
+    }
 }
